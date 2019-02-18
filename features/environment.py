31f616fd853150b010d1963d8e4befd26026930d
Chutchev <iChutchev98@gmail.com>
2019-02-18 22:55:27 +0500

    После завершения теста будут удаляться скриншоты

--- features/environment.py
+++ features/environment.py
@@ -4,3 +4,9 @@ import os
 def before_all(context):
     context.browser = webdriver.Chrome(os.path.abspath("chromedriver.exe"))
     context.browser.maximize_window()
+
+
+def after_all(context):
+    screens_directory = os.path.abspath("Screenshots")
+    for image in os.listdir(screens_directory):
+        os.remove(os.path.join(screens_directory, image))
