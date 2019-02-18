fb4d5871394ec4c6d7346a3406b359435339d781
Chutchev <iChutchev98@gmail.com>
2019-02-18 23:34:20 +0500

    Добавлено несколько функций

--- features/lib/pages/cb_page.py
+++ features/lib/pages/cb_page.py
@@ -13,7 +13,12 @@ class CbPage(BasePage):
                                         'and contains(text(), "Интернет-приемная")]'),
         'Написать благодарность': (By.XPATH, '//*[@href="/Reception/Message/Register?messageType=Gratitude"]'),
         'Ваша благодарность': (By.XPATH, '//*[@name="MessageBody"]'),
-        'Я согласен': (By.XPATH, '//*[@type="checkbox" and contains(@data-title, "Согласие")]')
+        'Я согласен': (By.XPATH, '//*[@type="checkbox" and contains(@data-title, "Согласие")]'),
+        'Три полоски': (By.XPATH, '//div[@class="header__main"]/span/span[@class="burger"]'),
+        'О сайте': (By.XPATH, '//li[@class="for_branch_11377"]/a[@href="/About/"]'),
+        'Предупреждение': (By.XPATH, '//li[@data-catalogid="11380"]/div/a[contains(text(), "Предупреждение")]'),
+        'Текст предупреждения': (By.XPATH, '//*[@id="content"]/p'),
+        'en': (By.XPATH, '//li/a[@href="/Localization/SwitchLanguage?url=%2FAbout%2Fwarning%2F&from=ru-RU&to=en-GB"]')
     }
 
     def __init__(self, context):
@@ -34,4 +39,25 @@ class CbPage(BasePage):
     def check(self, chekcbox_text):
         element = WebDriverWait(self.browser, 20).until(
             EC.presence_of_element_located(self.locator[chekcbox_text])
+        ).click()
+
+    def menu(self, button):
+        element = WebDriverWait(self.browser, 20).until(
+            EC.presence_of_element_located(self.locator[button])
+        ).click()
+
+    def open_section(self, section):
+        element = WebDriverWait(self.browser, 20).until(
+            EC.presence_of_element_located(self.locator[section])
+        ).click()
+
+    def alarm_text(self):
+        element = WebDriverWait(self.browser, 20).until(
+            EC.presence_of_element_located(self.locator['Текст предупреждения'])
+        )
+        return element.text
+
+    def change_language(self, language):
+        element = WebDriverWait(self.browser, 20).until(
+            EC.presence_of_element_located(self.locator[language])
         ).click()
