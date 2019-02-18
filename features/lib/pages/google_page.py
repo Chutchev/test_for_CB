811c8d0ebbfdd418b89b20a71c8cc154a3a751cb
Chutchev <iChutchev98@gmail.com>
2019-02-18 22:04:09 +0500

    Класс страницы "https://google.ru" для паттерна Page Object

---
+++ features/lib/pages/google_page.py
@@ -0,0 +1,54 @@
+from selenium.webdriver.common.by import By
+from features.lib.pages.base_page_object import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+import time
+
+class GooglePage(BasePage):
+
+    locator = {
+        'поиск': (By.XPATH, "//*[@class='gLFyf gsfi']"),
+        'Поиск в Google': (By.XPATH, "//*[@class='VlcLAe']/center/input[@value='Поиск в Google']"),
+        "cbr.ru": (By.XPATH, "//*[@class='iUh30' and contains(text(), 'cbr')]")
+    }
+
+    def __init__(self, context):
+        BasePage.__init__(self, context.browser)
+
+    def fill_find_field(self, text):
+        self.find_element(*self.locator['поиск']).send_keys(text)
+
+    def check_field(self, what):
+        if what in self.locator.keys():
+            element = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located(self.locator[what])
+            )
+            return True
+        else: return False
+
+    def find_info(self, button_name):
+        try:
+            element = WebDriverWait(self.browser, 20).until(
+                EC.presence_of_element_located(self.locator[button_name])
+            )
+            element.click()
+        except ElementNotVisibleException:
+            time.sleep(5)
+            element = WebDriverWait(self.browser, 20).until(
+                EC.presence_of_element_located(self.locator[button_name])
+            )
+            element.click()
+
+    def find_url(self, url):
+        element = WebDriverWait(self.browser, 20).until(
+            EC.presence_of_element_located(self.locator[url])
+        )
+        if element:
+            print("Найдено")
+        else:
+            print("Не найдено")
+
+
+
+
